a4fe36323725f3e57ea07d463f5fe7306039cb99
Robson Leal <robson_leal@outlook.com.br>
2022-11-22 02:39:45 -0300

    Criando método para copiar atributos de um Dto para uma entidade e corrigindo os métodos de update e insert

--- server/src/main/java/com/desbugando/catalogo/services/ProductService.java
+++ server/src/main/java/com/desbugando/catalogo/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.desbugando.catalogo.services;
 
+import com.desbugando.catalogo.dtos.CategoryDto;
 import com.desbugando.catalogo.dtos.ProductDto;
+import com.desbugando.catalogo.entities.Category;
 import com.desbugando.catalogo.entities.Product;
+import com.desbugando.catalogo.repositories.CategoryRepository;
 import com.desbugando.catalogo.repositories.ProductRepository;
 import com.desbugando.catalogo.services.exceptions.DatabaseException;
 import com.desbugando.catalogo.services.exceptions.ResourceNotFoundException;
@@ -18,10 +21,13 @@ import javax.persistence.EntityNotFoundException;
 @Service
 public class ProductService {
 
-    private String mensagemErroCategoria404 = "Categoria não encontrada";
+    private final String MESSAGE_NOT_FOUND_CATEGORY = "Categoria não encontrada";
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public Page<ProductDto> findAllPaged(PageRequest pageRequest) {
@@ -33,7 +39,7 @@ public class ProductService {
     @Transactional(readOnly = true)
     public ProductDto findById(Long id) {
         Product entity = repository.findById(id).orElseThrow(
-            () -> new ResourceNotFoundException(mensagemErroCategoria404)
+            () -> new ResourceNotFoundException(MESSAGE_NOT_FOUND_CATEGORY)
         );
         
         return new ProductDto(entity, entity.getCategories());
@@ -42,7 +48,7 @@ public class ProductService {
     @Transactional
     public ProductDto insert(ProductDto dto) {
         Product entity = new Product();
-        // entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         return new ProductDto(repository.save(entity));
     }
 
@@ -50,10 +56,10 @@ public class ProductService {
     public ProductDto update(Long id, ProductDto dto) {
         try {
             Product entity = repository.getReferenceById(id);
-            // entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             return new ProductDto(repository.save(entity));
         } catch (EntityNotFoundException e) {
-            throw new ResourceNotFoundException(mensagemErroCategoria404);
+            throw new ResourceNotFoundException(MESSAGE_NOT_FOUND_CATEGORY);
         }
     }
 
@@ -61,9 +67,23 @@ public class ProductService {
         try {
             repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
-            throw new ResourceNotFoundException(mensagemErroCategoria404);
+            throw new ResourceNotFoundException(MESSAGE_NOT_FOUND_CATEGORY);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Violação da integridade do banco de dados");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDto dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setDate(dto.getDate());
+
+        entity.getCategories().clear();
+        for(CategoryDto catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
         }
     }
 }

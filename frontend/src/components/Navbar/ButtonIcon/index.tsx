801cf19af969d8bfaf4c2bd92d9fe67f3350be64
Robson Leal <robson_leal@sicredi.com.br>
2023-09-18 19:36:32 -0300

    Iniciando a home e criando um componente de botão

---
+++ frontend/src/components/Navbar/ButtonIcon/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+export const ButtonIcon = () => {
+  return (
+    <button className="btn btn-primary btn-icon">
+      <h6 className="text-uppercase">Inicie agora a sua busca</h6>
+    </button>
+  );
+}

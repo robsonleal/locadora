53f23d4be9e646e2610ff5ac0eee9ca7c35a042e
Robson Leal <robson_leal@sicredi.com.br>
2023-09-19 14:49:42 -0300

    Construindo pagina de catalogo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import 'assets/styles/custom.scss';
 import './App.css';
 import { Home } from 'pages/Home';
+import { Catalogo } from 'pages/Catalogo';
 
 const App = () => {
   return (
-    <Home />
+    <Catalogo />
   );
 };
 

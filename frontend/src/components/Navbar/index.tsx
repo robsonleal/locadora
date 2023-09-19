d681c1e403a91d6b6ef649bdd5844a27b50f688b
Robson Leal <robson_leal@sicredi.com.br>
2023-09-19 15:57:29 -0300

    Configurando rotas do projeto

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,14 +1,18 @@
 import './styles.scss';
 import '@popperjs/core';
 import 'bootstrap/js/src/collapse';
+import { NavLink, Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand fw-bolder d-lg-none d-flex align-items-center ms-3">
+        <Link
+          to="/"
+          className="navbar-brand fw-bolder d-lg-none d-flex align-items-center ms-3"
+        >
           <h4>Locadora</h4>
-        </a>
+        </Link>
 
         <button
           className="navbar-toggler ms-auto"
@@ -23,24 +27,33 @@ export const Navbar = () => {
         </button>
 
         <div className="collapse navbar-collapse d-lg-flex" id="navbarCollapse">
-          <a className="navbar-brand d-none d-lg-flex fw-bolder me-0">
+          <Link to="/" className="navbar-brand d-none d-lg-flex fw-bolder me-0">
             <h4>Locadora</h4>
-          </a>
+          </Link>
           <ul className="navbar-nav mx-lg-auto">
             <li className="nav-item">
-              <a className="nav-link text-uppercase fw-bolder fs-6 active">
+              <NavLink
+                to="/"
+                className={({ isActive }) => `nav-link text-uppercase fw-bolder fs-6 ${isActive ? 'active' : '' }`}
+              >
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-uppercase fw-bolder fs-6" href="#">
+              <NavLink
+                to="/filmes"
+                className="nav-link text-uppercase fw-bolder fs-6"
+              >
                 Catálogo
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-uppercase fw-bolder fs-6" href="#">
+              <NavLink
+                to="/admin"
+                className="nav-link text-uppercase fw-bolder fs-6"
+              >
                 Admin
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>

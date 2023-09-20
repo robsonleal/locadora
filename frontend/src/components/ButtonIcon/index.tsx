25315e62bc296a0c7cdabbf91d7fe01ad2d3558c
Robson Leal <robson_leal@sicredi.com.br>
2023-09-19 22:56:13 -0300

    [frontend] Contruindo componente ButtonIcon

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,9 +1,16 @@
-import './styles.scss'
+import './styles.scss';
+
+import { ReactComponent as ArrowIcon } from 'assets/images/seta.svg';
 
 export const ButtonIcon = () => {
   return (
-    <button className="btn btn-primary btn-icon">
-      <h6 className="text-uppercase">Inicie agora a sua busca</h6>
-    </button>
+    <div className="btn-container btn btn-primary">
+      <div className='btn-text-container'>
+        <h6 className="text-uppercase">Inicie agora a sua busca</h6>
+      </div>
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
   );
-}
+};

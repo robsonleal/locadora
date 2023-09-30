663a038e5f8aca760e7b0dc53bc8e0716108212d
Robson Leal <robson_leal@sicredi.com.br>
2023-09-30 11:29:48 -0300

    Atualizando projeto

--- server/src/main/java/com/desbugando/catalogo/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.desbugando.catalogo.repositories;
-
-import com.desbugando.catalogo.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
